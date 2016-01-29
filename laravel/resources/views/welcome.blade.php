7d7a39e2a25e571e0d86d13bef0e371f97f9c220
suyong <cooksang@gmail.com>
2016-01-30 04:20:11 +0900

    4주차

--- laravel/resources/views/welcome.blade.php
+++ laravel/resources/views/welcome.blade.php
@@ -1,23 +1,23 @@
 <!doctype html>
 <html>
 <head>
-<title>·¹ÀÌ¾Æ¿ô</title>
+<title>layout</title>
+<meta charset="utf-8">
 <style type="text/css">
-html{
+html, body{
 	margin:0 auto;
 	padding: 0;
 	width:100%;
 	height:100%;
 }
-body{
+#wrap{
 	margin:0 auto;
 	padding: 0;
 	background-color: green;
 	color: #FFF;
 	width: 100%;
-	height: 96%;
+	height:100%;
 	text-align: center;
-	overflow: hidden;
 	font-size: 22px;
 }
 header{
@@ -30,20 +30,18 @@ footer{
 	font-size: 20px;
 	width: 100%;
 	height: 3%;
-	position: fixed;
 	bottom: 40px;
 }
-header span, footer span{
+span{
 	padding: 20px 0;
 	width: 33.333333%;
 	float: left;
 	height: 100%;
-
 }
 article{
 	padding: 0;
 	width: 100%;
-	height: 90%;
+	min-height: 86.1%;
 	font-size:19px;
 }
 ol{
@@ -90,7 +88,6 @@ div{
 }
 .center{
 	margin: 0 auto;
-	height: 100%;
 }
 .contents{
 	width: 50%;
@@ -102,119 +99,119 @@ div{
 
 </head>
 <body>
-	<header>
-		<span class="black"></span>
-		<span class="gray">Header</span>
-		<span class="purple"></span>
-	</header>
-	<article class="float_left yellow">
-		<div class="center orange">
-			<div class="float_left purple sidebar">
-				<h3>Sidebar1</h3>
-				<ol class="float_left blue">
-					<li>Sidebar1 Menu</li>
-					<li>Sidebar1 Menu</li>
-					<li>Sidebar1 Menu</li>
-					<li>Sidebar1 Menu</li>
-					<li>Sidebar1 Menu</li>
-					<li>Sidebar1 Menu</li>
-					<li>Sidebar1 Menu</li>
-					<li>Sidebar1 Menu</li>
-					<li>Sidebar1 Menu</li>
-					<li>Sidebar1 Menu</li>
-					<li>Sidebar1 Menu</li>
-					<li>Sidebar1 Menu</li>
-					<li>Sidebar1 Menu</li>
-					<li>Sidebar1 Menu</li>
-					<li>Sidebar1 Menu</li>
-					<li>Sidebar1 Menu</li>
-					<li>Sidebar1 Menu</li>
-					<li>Sidebar1 Menu</li>
-					<li>Sidebar1 Menu</li>
-					<li>Sidebar1 Menu</li>
-					<li>Sidebar1 Menu</li>
-					<li>Sidebar1 Menu</li>
-					<li>Sidebar1 Menu</li>
-					<li>Sidebar1 Menu</li>
-					<li>Sidebar1 Menu</li>
-					<li>Sidebar1 Menu</li>
-					<li>Sidebar1 Menu</li>
-					<li>Sidebar1 Menu</li>
-					<li>Sidebar1 Menu</li>
-					<li>Sidebar1 Menu</li>
-					<li>Sidebar1 Menu</li>
-					<li>Sidebar1 Menu</li>
-					<li>Sidebar1 Menu</li>
-					<li>Sidebar1 Menu</li>
-					<li>Sidebar1 Menu</li>
-					<li>Sidebar1 Menu</li>
-					<li>Sidebar1 Menu</li>
-					<li>Sidebar1 Menu</li>
-					<li>Sidebar1 Menu</li>
-					<li>Sidebar1 Menu</li>
-					<li>Sidebar1 Menu</li>
-					<li>Sidebar1 Menu</li>
-					<li>Sidebar1 Menu</li>
-					<li>Sidebar1 Menu</li>
-					<li>Sidebar1 Menu</li>
-					<li>Sidebar1 Menu</li>
-					<li>Sidebar1 Menu</li>
-					<li>Sidebar1 Menu</li>
-					<li>Sidebar1 Menu</li>
-					<li>Sidebar1 Menu</li>
-					<li>Sidebar1 Menu</li>
-					<li>Sidebar1 Menu</li>
-					<li>Sidebar1 Menu</li>
-					<li>Sidebar1 Menu</li>
-					<li>Sidebar1 Menu</li>
-					<li>Sidebar1 Menu</li>
-					<li>Sidebar1 Menu</li>
-					<li>Sidebar1 Menu</li>
-					<li>Sidebar1 Menu</li>
-					<li>Sidebar1 Menu</li>
-				</ol>
+	<div id="wrap">
+		<header>
+			<span class="black"></span>
+			<span class="gray">Header</span>
+			<span class="purple"></span>
+		</header>
+		<article class="float_left yellow">
+			<div id="orange" class="center orange">
+				<div class="float_left purple sidebar">
+					<h3>Sidebar1</h3>
+					<ol class="float_left blue">
+						<li>Sidebar1 Menu</li>
+						<li>Sidebar1 Menu</li>
+						<li>Sidebar1 Menu</li>
+						<li>Sidebar1 Menu</li>
+						<li>Sidebar1 Menu</li>
+						<li>Sidebar1 Menu</li>
+						<li>Sidebar1 Menu</li>
+						<li>Sidebar1 Menu</li>
+					</ol>
+				</div>
+				<div class="float_left darkblue contents">
+					<h3>Content</h3>
+					<ul class="float_left brown">
+						<li>CSS stands for Cascading Style Sheet.</li>
+						<li>CSS stands for Cascading Style Sheet.</li>
+						<li>CSS stands for Cascading Style Sheet.</li>
+						<li>CSS stands for Cascading Style Sheet.</li>
+						<li>CSS stands for Cascading Style Sheet.</li>
+						<li>CSS stands for Cascading Style Sheet.</li>
+						<li>CSS stands for Cascading Style Sheet.</li>
+						<li>CSS stands for Cascading Style Sheet.</li>
+						<li>CSS stands for Cascading Style Sheet.</li>
+						<li>CSS stands for Cascading Style Sheet.</li>
+						<li>CSS stands for Cascading Style Sheet.</li>
+						<li>CSS stands for Cascading Style Sheet.</li>
+						<li>CSS stands for Cascading Style Sheet.</li>
+						<li>CSS stands for Cascading Style Sheet.</li>
+						<li>CSS stands for Cascading Style Sheet.</li>
+						<li>CSS stands for Cascading Style Sheet.</li>
+						<li>CSS stands for Cascading Style Sheet.</li>
+						<li>CSS stands for Cascading Style Sheet.</li>
+						<li>CSS stands for Cascading Style Sheet.</li>
+						<li>CSS stands for Cascading Style Sheet.</li>
+						<li>CSS stands for Cascading Style Sheet.</li>
+						<li>CSS stands for Cascading Style Sheet.</li>
+						<li>CSS stands for Cascading Style Sheet.</li>
+						<li>CSS stands for Cascading Style Sheet.</li>
+						<li>CSS stands for Cascading Style Sheet.</li>
+						<li>CSS stands for Cascading Style Sheet.</li>
+						<li>CSS stands for Cascading Style Sheet.</li>
+						<li>CSS stands for Cascading Style Sheet.</li>
+						<li>CSS stands for Cascading Style Sheet.</li>
+						<li>CSS stands for Cascading Style Sheet.</li>
+						<li>CSS stands for Cascading Style Sheet.</li>
+						<li>CSS stands for Cascading Style Sheet.</li>
+						<li>CSS stands for Cascading Style Sheet.</li>
+						<li>CSS stands for Cascading Style Sheet.</li>
+						<li>CSS stands for Cascading Style Sheet.</li>
+						<li>CSS stands for Cascading Style Sheet.</li>
+						<li>CSS stands for Cascading Style Sheet.</li>
+						<li>CSS stands for Cascading Style Sheet.</li>
+						<li>CSS stands for Cascading Style Sheet.</li>
+						<li>CSS stands for Cascading Style Sheet.</li>
+						<li>CSS stands for Cascading Style Sheet.</li>
+						<li>CSS stands for Cascading Style Sheet.</li>
+						<li>CSS stands for Cascading Style Sheet.</li>
+						<li>CSS stands for Cascading Style Sheet.</li>
+						<li>CSS stands for Cascading Style Sheet.</li>
+						<li>CSS stands for Cascading Style Sheet.</li>
+						<li>CSS stands for Cascading Style Sheet.</li>
+						<li>CSS stands for Cascading Style Sheet.</li>
+						<li>end</li>
+					</ul>
+				</div>
+				<div class="float_left black sidebar">
+					<h3>sidebar2</h3>
+					<ol class="float_left blue" type="A">
+						<li>Sidebar2 Menu</li>
+						<li>Sidebar2 Menu</li>
+						<li>Sidebar2 Menu</li>
+						<li>Sidebar2 Menu</li>
+						<li>Sidebar2 Menu</li>
+					</ol>
+				</div>
 			</div>
-			<div class="float_left darkblue contents">
-				<h3>Content</h3>
-				<ul class="float_left brown">
-					<li>CSS stands for Cascading Style Sheet.</li>
-					<li>CSS stands for Cascading Style Sheet.</li>
-					<li>CSS stands for Cascading Style Sheet.</li>
-					<li>CSS stands for Cascading Style Sheet.</li>
-					<li>CSS stands for Cascading Style Sheet.</li>
-					<li>CSS stands for Cascading Style Sheet.</li>
-					<li>CSS stands for Cascading Style Sheet.</li>
-					<li>CSS stands for Cascading Style Sheet.</li>
-					<li>CSS stands for Cascading Style Sheet.</li>
-					<li>CSS stands for Cascading Style Sheet.</li>
-					<li>CSS stands for Cascading Style Sheet.</li>
-					<li>CSS stands for Cascading Style Sheet.</li>
-					<li>CSS stands for Cascading Style Sheet.</li>
-					<li>CSS stands for Cascading Style Sheet.</li>
-					<li>CSS stands for Cascading Style Sheet.</li>
-					<li>CSS stands for Cascading Style Sheet.</li>
-					<li>CSS stands for Cascading Style Sheet.</li>
-					<li>CSS stands for Cascading Style Sheet.</li>
-					<li>CSS stands for Cascading Style Sheet.</li>
-					<li>CSS stands for Cascading Style Sheet.</li>
-				</ul>
-			</div>
-			<div class="float_left black sidebar">
-				<h3>sidebar2</h3>
-				<ol class="float_left blue" type="A">
-					<li>Sidebar2 Menu</li>
-					<li>Sidebar2 Menu</li>
-					<li>Sidebar2 Menu</li>
-					<li>Sidebar2 Menu</li>
-					<li>Sidebar2 Menu</li>
-				</ol>
-			</div>
-		</div>
-	</article>
-	<footer>
-		<span class="purple"></span>
-		<span class="gray">Footer</span>
-		<span class="black"></span>
-	</footer>
+		</article>
+		<footer>
+			<span class="purple"></span>
+			<span class="gray">Footer</span>
+			<span class="black"></span>
+		</footer>
+	</div>
 </body>
+<script type="text/javascript">
+	var heightSetting = function(e){
+		var orange = document.getElementById("orange");
+		orange.style.height = null;
+
+		var article = document.getElementsByTagName("article")[0];
+		var wrap = document.getElementById("wrap");
+		var header = document.getElementsByTagName("header")[0].getElementsByTagName("span")[1];
+		var footer = document.getElementsByTagName("footer")[0].getElementsByTagName("span")[1];
+		wrap.style.height = window.innerHeight + "px";
+		orange.style.height = article.clientHeight + "px";
+	}
+	
+	window.onload = function(e){
+		heightSetting(e);
+		window.addEventListener('resize', function(e){
+			heightSetting(e);
+		})
+	}
+	
+</script>
 </html>
